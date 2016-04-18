8ea6538e2b2b92db76ea01737fa56cb6e98ed162
pazdyga@pythian.com <pazdyga@pythian.com>
2016-04-18 07:23:16 +0200

    Extended custom funct a bit

--- modules/master/files/customfunct.rb
+++ modules/master/files/customfunct.rb
@@ -3,7 +3,7 @@ module Puppet::Parser::Functions
     filename = args[0]
     test = args[1]
     userid = `id`
-    date = `date "+%Y-%m-%d %H:%M:%S"`
+    date = `date "+%Y-%m-%d %H:%M:%S" | tr '\n' ' '`
     str = "[" + date + "] " + "This is a test: " + test + ". Here's the id info: " + userid
     File.open(filename, 'a') {|fd| fd.puts str }
   end
